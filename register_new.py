0da89b3d3427c1583fc54c9d71dca5d8de2afb6e
NeatStranger <ashtonm937@gmail.com>
2025-04-11 14:48:46 -0700

    Made Register Into Function

--- register_new.py
+++ register_new.py
@@ -1,15 +1,12 @@
 from db_functions import setupPG
 
-
-db, cursor = setupPG()
-SQL = "INSERT INTO packs DEFAULT VALUES returning id;"
-
-cursor.execute(SQL, )
-
-new_id = cursor.fetchone()[0]
-db.commit()
-
-print("Inserted row ID: ", new_id)
-
-cursor.close()
-db.close()
+def addNewPack():
+    db, cursor = setupPG()
+    SQL = "INSERT INTO packs DEFAULT VALUES returning id;"
+    cursor.execute(SQL, )
+    new_id = cursor.fetchone()[0]
+    db.commit()
+    print("Inserted row ID: ", new_id)
+    cursor.close()
+    db.close()
+    return new_id
