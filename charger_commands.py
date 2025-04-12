afe15ed9bd51ef2a827976483902ae995f56820f
neatstranger <39034037+neatstranger@users.noreply.github.com>
2025-04-11 21:54:50 -0700

    Update

--- charger_commands.py
+++ charger_commands.py
@@ -2,9 +2,6 @@
 def getBatSerial(ser):
     command = "G000A;"
     ser.write(command.encode('utf-8'))
-    
     response = ser.readline()
     batSerial =  response.decode('utf-8').strip()
-    print("Received: ", batSerial) 
-    
 
